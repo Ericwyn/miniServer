513a4e87e74fcbecefc9c890a6b5d17b14696809
Ericwyn <Ericwyn.chen@gmail.com>
2018-06-28 23:47:46 +0800

    增加了对自定义端口的支持,启动参数为 -p ，默认端口为 10010

--- miniServer.go
+++ miniServer.go
@@ -6,17 +6,21 @@ import (
 	"path/filepath"
 	"os"
 	"strings"
+	"flag"
 )
 func main() {
 	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
 		log.Fatal(err)
 	}
+	port := flag.String("p", "10010", "http listen port")
 	dirPath := strings.Replace(dir, "\\", "/", -1)
 	fmt.Println(dirPath)
 	h := http.FileServer(http.Dir(dirPath))
-	fmt.Println("服务启动在 10010")
-	err2 := http.ListenAndServe(":10010", h)
+	flag.Parse()
+	ports := ":"+*port
+	fmt.Println("服务启动在"+ports)
+	err2 := http.ListenAndServe(ports, h)
 	if err2 != nil {
 		log.Fatal("ListenAndServe: ", err2)
 	}
