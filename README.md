7d8419e4f59994956752f8b4c47ef8ed33c093e1
Ericwyn <Ericwyn.chen@gmail.com>
2018-07-13 11:50:23 +0800

    加入了对 miniServer 进程的管理功能

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # miniServer_GO
-用 Go 写的一个小工具，虽然很简单，但是可能对大家有点帮助
+为当前目录开启一个静态服务器
 
-## 喵喵喵???
-自己平常里经常用到，在某个文件夹下运行的时候，直接就可以把这个文件夹变成静态服务器的根目录，不需要配置 Caddy 之类的，因为本地调试 javaScript 的时候会受到浏览器限制，例如 Chrome 是不允许 js 直接通过 ajax 读取本地文件的。而如果是运行在 localhost:XXXX 上面的网页的话就可以
+## 使用教程
+ 
+    -p 设定静态服务器运行端口
+    -k [port number] 杀死特定端口上面的 miniServer
+    -kl 杀死所有正在运行的 miniServer 程序
 
-## 更多
- - 日常里面发现，例如 Axure PR 将原型图导出后的 HTML 页面，需要安装 Axure PR 的 chrome 插件才能运行，而如果将 miniServer 放到导出的 html 的文件夹目录下，点击运行之后，就可以直接通过 localhost:10010 这样的地址访问了
+注意:杀死进程依赖于 bash 里 `kill` 命令和 `netstat` 命令
+
+所以你可能需要安装 `net-tools` 
+
+## 日常用法
+ - 调试本地的静态网站工程和本地 Javascript( Chrome 是不允许 js 直接通过 ajax 读取本地文件的, 但是使用静态服务器开启网站的时候就可以)
+
+ - Axure PR 将原型图导出后的 HTML 页面，需要安装 Axure PR 的 chrome 插件才能运行，而如果将 miniServer 放到导出的 html 的文件夹目录下，点击运行之后，就可以直接通过 localhost:10010 这样的地址访问了
  
  - 或者是直接开启一个 wifi 分享，内网环境下其他的机器直接通过访问（本机ip地址:端口）的形式来下载文件
  
--- miniServer.go
+++ miniServer.go
@@ -7,21 +7,99 @@ import (
 	"os"
 	"strings"
 	"flag"
+	"os/exec"
+	"regexp"
+	"strconv"
 )
+
+var port = flag.String("p", "10010", "http listen port")
+var kPort = flag.Int("k",-1,"kill the miniServer running on this port")
+var killFlag = flag.Bool("kl",false,"kill the all running miniServer")
 func main() {
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		log.Fatal(err)
-	}
-	port := flag.String("p", "10010", "http listen port")
-	dirPath := strings.Replace(dir, "\\", "/", -1)
-	fmt.Println(dirPath)
-	h := http.FileServer(http.Dir(dirPath))
 	flag.Parse()
-	ports := ":"+*port
-	fmt.Println("服务启动在"+ports)
-	err2 := http.ListenAndServe(ports, h)
-	if err2 != nil {
-		log.Fatal("ListenAndServe: ", err2)
+	if *kPort != -1 {
+		if killAPort(*kPort) {
+			fmt.Println("成功停止运行端口号为",*port,"的 miniServer 进程")
+		}else {
+			fmt.Println("无法停止运行端口号为",*port,"的 miniServer 进程")
+		}
+		os.Exit(0)
+	}else if *killFlag {
+		killAllThread()
+		os.Exit(0)
+	}else {
+		dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+		if err != nil {
+			log.Fatal(err)
+		}
+		dirPath := strings.Replace(dir, "\\", "/", -1)
+		fmt.Println(dirPath)
+		h := http.FileServer(http.Dir(dirPath))
+		ports := ":"+*port
+		fmt.Println("服务启动在"+ports)
+		err2 := http.ListenAndServe(ports, h)
+		if err2 != nil {
+			log.Fatal("ListenAndServe: ", err2)
+		}
 	}
+}
+
+type Thread struct {
+	Port string
+	Pid string
+}
+
+func getAllRunning() ([]Thread) {
+	var cmdOutput []byte
+	var err error
+	var cmd *exec.Cmd
+
+	// 执行单个shell命令时, 直接运行即可
+	cmd = exec.Command("netstat","-nlp")
+	if cmdOutput, err = cmd.Output(); err != nil {
+		fmt.Println(err)
+		fmt.Println("netstate 命令调用错误")
+		os.Exit(1)
+	}
+	lines := strings.Split(string(cmdOutput),"\n")
+	portRegex := regexp.MustCompile(`:::[0-9]{1,5}`)
+	pidRegex := regexp.MustCompile(`[0-9]{1,5}\/miniServer`)
+	var resList []Thread
+	for _,line := range lines{
+		if strings.Contains(line,"miniServer") {
+			resList = append(resList,Thread{
+				Port:strings.Replace(portRegex.FindString(line),":","",-1),
+				Pid:strings.Replace(pidRegex.FindString(line),"/miniServer","",-1),
+			})
+		}
+	}
+	return resList
+}
+
+func killThread(thread Thread) bool {
+	var err error
+	var cmd *exec.Cmd
+	cmd = exec.Command("kill",thread.Pid)
+	if _, err = cmd.Output(); err != nil {
+		fmt.Println("kill 线程失败, 错误 :",err)
+		return false
+	}
+	fmt.Println("kill 端口",thread.Port,"上的 miniServer 进程")
+	return true
+}
+
+func killAllThread() {
+	for _,th := range getAllRunning(){
+		killThread(th)
+	}
+}
+
+func killAPort(port int) bool {
+	for _,th := range getAllRunning(){
+		if th.Port == strconv.Itoa(port) {
+			return killThread(th)
+		}
+	}
+	fmt.Println("不存在运行端口为",port,"的 miniServer 进程")
+	return false
 }
