a8b2d34a44688cbec02857d1c2f7e9f01ec21997
Ericwyn <Ericwyn.chen@gmail.com>
2018-07-13 16:41:09 +0800

    增加了列出所有 miniServer 功能

--- README.md
+++ README.md
@@ -6,10 +6,13 @@
     -p 设定静态服务器运行端口
     -k [port number] 杀死特定端口上面的 miniServer
     -kl 杀死所有正在运行的 miniServer 程序
+    -l 查看当前所有正在运行的 miniServer 
 
 注意:杀死进程依赖于 bash 里 `kill` 命令和 `netstat` 命令
 
 所以你可能需要安装 `net-tools` 
+
+并且 -p , -k , -kl , 功能也无法再 windows 系统上面使用
 
 ## 日常用法
  - 调试本地的静态网站工程和本地 Javascript( Chrome 是不允许 js 直接通过 ajax 读取本地文件的, 但是使用静态服务器开启网站的时候就可以)
@@ -19,5 +22,14 @@
  - 或者是直接开启一个 wifi 分享，内网环境下其他的机器直接通过访问（本机ip地址:端口）的形式来下载文件
  
 ## 编译
-直接 `go build miniServer.go` 就可以了，交叉编译的话在语句前面加上平台限定就好了，参考 Norcia.go 的编译 sh
+直接 
+
+    go build miniServer.go
+
+就可以了，交叉编译的话在语句前面加上平台限定就好了，参考 Norcia.go 的编译 sh
  
+如果要方便的话就
+    
+    sudo cp miniServer /usr/bin
+
+这样就直接变成一个 bash 命令了
--- miniServer.go
+++ miniServer.go
@@ -15,6 +15,9 @@ import (
 var port = flag.String("p", "10010", "http listen port")
 var kPort = flag.Int("k",-1,"kill the miniServer running on this port")
 var killFlag = flag.Bool("kl",false,"kill the all running miniServer")
+var list = flag.Bool("l",false,"list the status of all running miniServer")
+var dir = flag.String("d","null","the dir path the miniServer listen, default is current path")
+
 func main() {
 	flag.Parse()
 	if *kPort != -1 {
@@ -25,31 +28,55 @@ func main() {
 		}
 		os.Exit(0)
 	}else if *killFlag {
-		killAllThread()
+		killAllProcess()
 		os.Exit(0)
-	}else {
-		dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-		if err != nil {
-			log.Fatal(err)
+	}else if *list {
+		fmt.Println("运行端口"+"\t"+"进程id"+"\t\t"+"监听位置")
+		for _,thread := range getAllRunning() {
+			fmt.Println(thread.Port+"\t\t"+thread.Pid+"\t\t"+thread.DirPath)
 		}
-		dirPath := strings.Replace(dir, "\\", "/", -1)
-		fmt.Println(dirPath)
-		h := http.FileServer(http.Dir(dirPath))
-		ports := ":"+*port
-		fmt.Println("服务启动在"+ports)
-		err2 := http.ListenAndServe(ports, h)
-		if err2 != nil {
-			log.Fatal("ListenAndServe: ", err2)
+	}else {
+		if *dir == "null" {
+			fmt.Println("未使用 -d 指定监听路径")
+			runInNewProcress()
+		}else {
+			dirPath := strings.Replace(*dir, "\\", "/", -1)
+			fmt.Println("监听:"+dirPath)
+			h := http.FileServer(http.Dir(dirPath))
+			ports := ":"+*port
+			fmt.Println("服务启动在"+ports)
+			err2 := http.ListenAndServe(ports, h)
+			if err2 != nil {
+				log.Fatal("ListenAndServe: ", err2)
+			}
 		}
 	}
 }
 
-type Thread struct {
-	Port string
-	Pid string
+// 获取执行文件夹
+func getCurPath() string {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return dir
 }
 
-func getAllRunning() ([]Thread) {
+// 获取可执行文件的绝对路径
+func GetBinPath() string {
+	file, _ := exec.LookPath(os.Args[0])
+	path, _ := filepath.Abs(file)
+	rst := filepath.Dir(path)
+	return rst
+}
+
+type Process struct {
+	Port string
+	Pid string
+	DirPath string
+}
+
+func getAllRunning() ([]Process) {
 	var cmdOutput []byte
 	var err error
 	var cmd *exec.Cmd
@@ -64,42 +91,88 @@ func getAllRunning() ([]Thread) {
 	lines := strings.Split(string(cmdOutput),"\n")
 	portRegex := regexp.MustCompile(`:::[0-9]{1,5}`)
 	pidRegex := regexp.MustCompile(`[0-9]{1,5}\/miniServer`)
-	var resList []Thread
+	var resList []Process
 	for _,line := range lines{
 		if strings.Contains(line,"miniServer") {
-			resList = append(resList,Thread{
+			pid := strings.Replace(pidRegex.FindString(line),"/miniServer","",-1)
+			resList = append(resList, Process{
 				Port:strings.Replace(portRegex.FindString(line),":","",-1),
-				Pid:strings.Replace(pidRegex.FindString(line),"/miniServer","",-1),
+				Pid: pid,
+				DirPath:getListenDir(pid),
 			})
 		}
 	}
 	return resList
 }
 
-func killThread(thread Thread) bool {
+func getListenDir(pid string) string {
+	var cmdOutput []byte
 	var err error
 	var cmd *exec.Cmd
-	cmd = exec.Command("kill",thread.Pid)
+
+	// 执行单个shell命令时, 直接运行即可
+	cmd = exec.Command("ps","-efH")
+	if cmdOutput, err = cmd.Output(); err != nil {
+		fmt.Println(err)
+		fmt.Println("netstate 命令调用错误")
+		os.Exit(1)
+	}
+	lines := strings.Split(string(cmdOutput),"\n")
+	for _,line := range lines{
+		if strings.Contains(line,pid) &&
+			strings.Contains(line, "miniServer") && strings.Contains(line, "-d") {
+				temp := strings.Split(line,"-d")[1]
+				if strings.Contains(temp,"-p") {
+					temp = strings.Split(temp,"-p")[0]
+				}
+				return strings.Replace(temp," ","",-1)
+		}
+	}
+	return "null"
+}
+
+func killProcess(process Process) bool {
+	var err error
+	var cmd *exec.Cmd
+	cmd = exec.Command("kill", process.Pid)
 	if _, err = cmd.Output(); err != nil {
 		fmt.Println("kill 线程失败, 错误 :",err)
 		return false
 	}
-	fmt.Println("kill 端口",thread.Port,"上的 miniServer 进程")
+	fmt.Println("kill 端口", process.Port,"上的 miniServer 进程")
 	return true
 }
 
-func killAllThread() {
+func killAllProcess() {
 	for _,th := range getAllRunning(){
-		killThread(th)
+		killProcess(th)
 	}
 }
 
 func killAPort(port int) bool {
 	for _,th := range getAllRunning(){
 		if th.Port == strconv.Itoa(port) {
-			return killThread(th)
+			return killProcess(th)
 		}
 	}
 	fmt.Println("不存在运行端口为",port,"的 miniServer 进程")
 	return false
+}
+
+// 在新的线程里面开启
+func runInNewProcress() {
+	var cmd *exec.Cmd
+	if *port != "10010" {
+		fmt.Println("使用命令:\n\t",GetBinPath()+"/miniServer","-d",getCurPath(),"-p",*port,"\n创建新进程")
+		fmt.Println("服务运行在: "+*port+"端口")
+		cmd = exec.Command(GetBinPath()+"/miniServer","-d",getCurPath(),"-p",*port)
+	}else {
+		fmt.Println("使用命令:\n\t",GetBinPath()+"/miniServer","-d",getCurPath(),"\n创建新进程")
+		fmt.Println("服务运行在: 10010 端口")
+		cmd = exec.Command(GetBinPath()+"/miniServer","-d",getCurPath())
+	}
+	_, err := cmd.Output()
+	if err != nil {
+		fmt.Println("创建新线程失败或遭退出, :",err)
+	}
 }
